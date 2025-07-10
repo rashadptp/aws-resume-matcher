c1930361fefa0fcf0cd4cb37fb0dba465800624a
junaiddata <junaiddataanalyst@gmail.com>
2025-07-10 11:31:08 +0400

    Update models.py

--- models.py
+++ models.py
@@ -10,14 +10,14 @@ class User(db.Model):
     password_hash = db.Column(db.String(128), nullable=False)
     matches_left = db.Column(db.Integer, default=10)
 
+    match_history = db.relationship("MatchHistory", backref="user", lazy=True)
+
     def check_password(self, password):
         return check_password_hash(self.password_hash, password)
 
 class MatchHistory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
-    resume_name = db.Column(db.String(256))
+    resume_name = db.Column(db.String(255))
     score = db.Column(db.Float)
-    timestamp = db.Column(db.DateTime, default=datetime.utcnow)
-
-    user = db.relationship('User', backref=db.backref('history', lazy=True))
+    timestamp = db.Column(db.DateTime)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False)
